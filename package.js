667de70d28ef663ab1a08a5aa735b60a1aa302cc
Simon Schick <simonsimcity@gmail.com>
2018-08-09 10:18:32 +0200

    Updated version to underscored version of the wrapped package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteortesting:mocha-core',
   summary: 'Fibers aware mocha server side wrappers. Internal package used by meteortesting:mocha.',
-  version: '1.0.2',
+  version: '5.2.0_1',
   testOnly: true,
   git: 'https://github.com/meteortesting/meteor-mocha-core.git'
 });
