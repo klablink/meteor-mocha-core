c44e49a11d59e4863bdb38d3527ebb6373c16b3c
Simon Schick <SimonSimCity@gmail.com>
2019-07-23 10:22:52 +0200

    Updated to Mocha 6.2.0 and released on Atmosphere

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'meteortesting:mocha-core',
   summary: 'Fibers aware mocha server side wrappers. Internal package used by meteortesting:mocha.',
-  version: '6.1.2',
+  version: '6.2.0',
   testOnly: true,
   git: 'https://github.com/meteortesting/meteor-mocha-core.git'
 });
 
 Npm.depends({
-  mocha: '6.1.2'
+  mocha: '6.2.0'
 });
 
 Package.onUse(function (api, where) {
