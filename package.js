b9a66ade3bff858348d5eedc06b188023f3c406b
d.ruggeroni <d.ruggeroni@lectra.com>
2024-08-05 12:39:18 +0200

    added junit reporter

--- package.js
+++ package.js
@@ -3,13 +3,14 @@
 Package.describe({
   name: 'meteortesting:mocha-core',
   summary: 'Mocha server side wrappers. Internal package used by meteortesting:mocha.',
-  version: '8.2.0',
+  version: '8.2.1',
   testOnly: true,
   git: 'https://github.com/meteortesting/meteor-mocha-core.git'
 })
 
 Npm.depends({
-  mocha: '10.2.0'
+  mocha: '10.2.0',
+  'mocha-junit-reporter': '2.2.1'
 })
 
 Package.onUse(function (api) {
